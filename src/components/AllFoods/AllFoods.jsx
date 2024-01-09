29f35ae5d38c0f28a48e6853cd3454758c874594
Mahin Hasan <mahinmtrs@gmail.com>
2024-01-10 04:45:58 +0600

    designed available foods

--- src/components/AllFoods/AllFoods.jsx
+++ src/components/AllFoods/AllFoods.jsx
@@ -1,14 +1,37 @@
+import { IoTimerOutline } from "react-icons/io5";
 
-const AllFoods = ({allFood}) => {
-    const {foodName,expiredDateTime} =allFood;
+
+
+const AllFoods = ({ allFood }) => {
+    const { foodImage, foodName, expiredDateTime, userName, userPhoto, foodQuantity, pickupLocation, additionalNotes } = allFood;
     return (
-        <div>
-            {/* <h1>this is All Foods</h1> */}
-            <p>Food Name: {foodName}</p>
-            <p>Validaty: {expiredDateTime} days remaining</p>
+        //    max-w-md
+        <div className="p-8 sm:flex items-center justify-evenly sm:space-x-6 bg-stone-100 text-gray-800 rounded-lg">
+            <div className="flex-shrink-0 w-full mb-6 h-48 sm:h-40 sm:w-40 sm:mb-0">
+                <img src={foodImage} alt="" className="object-cover object-center w-full h-full rounded bg-gray-500" />
+            </div>
+            <div className="flex flex-col space-y-1">
+                <div>
+                    <div className="flex items-center justify-end">
+                        <img className="w-9 rounded-full" src={userPhoto} alt="" />
+                        <h2 className="text-base font-semibold">{userName}</h2>
+                    </div>
+                    <h1 className="text-2xl custom-font tracking-wide text-amber-600 mt-1">{foodName}</h1>
+                </div>
+                <div className="space-y-1">
 
-            {/* Modal */}
-            
+                    <div className="text-stone-700">
+                        <p className="font-sans">Servings: {foodQuantity} Persons</p>
+                        <p className="font-sans">Pickup From: {pickupLocation}</p>
+                        <div className="flex items-center space-x-2">
+                            <p className="font-sans">Expires in: {expiredDateTime} </p>
+                            <IoTimerOutline className="text-amber-600 font-bold" />
+                        </div>
+
+                        <p className="font-sans">Additional Info: <br /><span className="italic font-mono tracking-tight">{additionalNotes}</span></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/AvailableFoods/AvailableFoods.jsx
+++ src/components/AvailableFoods/AvailableFoods.jsx
@@ -1,10 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import AllFoods from "../AllFoods/AllFoods";
 import { useState } from "react";
+import searchbanner from '../../assets/searchbanner.jpg'
 
 const AvailableFoods = () => {
     const allFoods = useLoaderData();
-    console.log(allFoods);
+    // console.log(allFoods);
 
     // Implement search functionality by food name
     const [searchedFoodName, setSearchedFoodName] = useState('');
@@ -22,28 +23,71 @@ const AvailableFoods = () => {
         setFoodName(e.target.value);
     }
     const foodNameLower = searchedFoodName.toLowerCase();
-    console.log('searchedFoodName:', searchedFoodName, 'foodName', foodName, 'foodNameLower', foodNameLower);
+    // console.log('searchedFoodName:', searchedFoodName, 'foodName', foodName, 'foodNameLower', foodNameLower);
     return (
-        <div>
+        <div className="container mx-auto">
             {/* Add some design */}
-            <h1>this is available food {allFoods.length}</h1>
+            {/* <h1>this is available food {allFoods.length}</h1>
             <div>
                 <form onSubmit={handleSubmit}>
                     <input onChange={handleSearchFood} className='input border rounded-s-md rounded-e-none' type="text" placeholder='Search Here...' />
                     <input onClick={() => handleSearchedFoodName(foodName)} className="btn rounded-e-md rounded-s-none text-white capitalize hover:bg-red-700 bg-[#FF444A]" type="submit" value="Search" />
                 </form>
-            </div>
-            {
-                allFoods.filter((item) => {
-                    return foodNameLower === ''
-                        ? item
-                        : item.foodName.toLowerCase() === foodNameLower;
-                }).map(allFood =>
-            <AllFoods key={allFood._id}
-                allFood={allFood}
+            </div> */}
+            <div
+                className="w-full bg-gray-500 mb-16"
+                style={{
+                    // backgroundImage: "url('https://source.unsplash.com/random/640x480')",
+                    backgroundImage: `url(${searchbanner})`,
+                    backgroundPosition: "center center",
+                    backgroundBlendMode: "multiply",
+                    backgroundSize: "cover",
+                }}
             >
-            </AllFoods>)
-            }
+                <div className="container flex flex-col flex-wrap content-center justify-center p-4 py-20 mx-auto md:p-36">
+                    <h1 className="text-6xl antialiased custom-font font-semibold leading text-center italic text-amber-500">
+                        Hungry..???
+                    </h1>
+                    <p className="pt-6 pb-8 text-xl antialiased text-center text-gray-100">
+                        Find Your Favourite Food
+                    </p>
+                    <div className="flex flex-row">
+                        {/* <input
+                            type="text"
+                            placeholder="Search Foods"
+                            className="w-3/5 p-3 rounded-l-lg sm:w-2/3"
+                        />
+                        <button
+                            type="button"
+                            className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 bg-amber-500 text-gray-50"
+                        >
+                            Search
+                        </button> */}
+                        <form onSubmit={handleSubmit}>
+                            <input onChange={handleSearchFood}
+                                type="text"
+                                placeholder="Search Foods"
+                                className="w-3/5 p-3 rounded-l-lg sm:w-2/3"
+                            />
+                            <input onClick={() => handleSearchedFoodName(foodName)} className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 bg-amber-500 hover:bg-amber-600 text-gray-50" type="submit" value="Search" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8">
+                {
+                    allFoods.filter((item) => {
+                        return foodNameLower === ''
+                            ? item
+                            : item.foodName.toLowerCase() === foodNameLower;
+                    }).map(allFood =>
+                        <AllFoods key={allFood._id}
+                            allFood={allFood}
+                        >
+                        </AllFoods>)
+                }
+            </div>
             {/* {
                 allFoods.map(allFood =>
                     <AllFoods key={allFood._id}
